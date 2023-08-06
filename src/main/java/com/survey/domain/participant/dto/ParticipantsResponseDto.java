537423d0a251c88c683aac8f612f0f0078c21a4d
penrose15 <seongju99@naver.com>
2023-08-06 15:42:27 +0900

    feat : create participant, respondent

---
+++ src/main/java/com/survey/domain/participant/dto/ParticipantsResponseDto.java
@@ -0,0 +1,15 @@
+package com.survey.domain.participant.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ParticipantsResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private Long surveyId;
+    private Integer number;
+}
---
+++ src/main/java/com/survey/domain/participant/repository/ParticipantsRepository.java
@@ -0,0 +1,26 @@
+package com.survey.domain.participant.repository;
+
+import com.survey.domain.participant.dto.ParticipantsResponseDto;
+import com.survey.domain.participant.entity.Participants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface ParticipantsRepository extends JpaRepository<Participants, Long> {
+
+    @Query("SELECT " +
+            "com.survey.domain.participant.dto.ParticipantsResponseDto(p.id, p.name, p.email, p.surveyId, p.number) " +
+            "FROM Participants p " +
+            "WHERE p.id = :id")
+    Optional<ParticipantsResponseDto> findByParticipantsId(Long id);
+
+    @Query("SELECT " +
+            "com.survey.domain.participant.dto.ParticipantsResponseDto(p.id, p.name, p.email, p.surveyId, p.number) " +
+            "FROM Participants p " +
+            "WHERE p.surveyId = :surveyId " +
+                "AND p.surveyDone = true ")
+    Page<ParticipantsResponseDto> findAllBySurveyIdAndSurveyDoneTrue(Long surveyId, Pageable pageable);
+}
